a800c08d58da55b08bbf9850310ca1c19e32ba9d
Daniel Cisternas <57823737+DcisternasGit@users.noreply.github.com>
2023-11-29 03:52:43 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,14 +1,9 @@
 function skillMember() {
-    var member = document.getElementById("member").value;
-    var memberError = document.getElementById("memberError");
-    var memberReg = /^[0-9]{1,2}$/;
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        location: 'Mars'
+    };
 
-    if (!memberReg.test(member)) {
-        memberError.textContent = "Por favor, ingresa un número válido (0-99).";
-        return false;
-    } else {
-        memberError.textContent = "";
-    }
-
-    return true;
+    return member;
 }
