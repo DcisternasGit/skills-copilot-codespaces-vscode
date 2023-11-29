c346b93713756a8ea94e21022e68b2437dc7fefb
Daniel Cisternas <57823737+DcisternasGit@users.noreply.github.com>
2023-11-29 04:01:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,40 @@
+// Create web server 
+// 1) GET /comments - return all comments
+// 2) POST /comments - create comment
+// 3) PUT /comments/:id - update comment
+// 4) DELETE /comments/:id - delete comment
 
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Comments } = require('./models');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/comments', (req, res) => {
+    Comments
+        .find()
+        .then(comments => {
+            res.json(comments.map(comment => comment.serialize()));
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'something went wrong' });
+        });
 });
 
-server.listen(3000, 'localhost', () => {
-  console.log('Server running at http://localhost:3000/');
-});
+app.post('/comments', (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing ${field} in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    Comments
+        .create({
+            title: req.body.title,
+            content: req.body.content,
